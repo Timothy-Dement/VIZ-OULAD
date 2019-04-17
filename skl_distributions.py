cd734d8f7c7e3c1e7d78bc7f92f4dc550c4dd760
Timothy-Dement <timothy.dement@gmail.com>
2019-04-17 01:10:07 -0400

    Distributions and summaries

---
+++ skl_distributions.py
@@ -0,0 +1,93 @@
+import os
+
+import pandas as pd
+import seaborn as sns
+
+from matplotlib import pyplot as plt
+from matplotlib import rcParams as rcp
+
+rcp.update({'figure.autolayout': True})
+rcp["figure.figsize"] = (10, 10)
+
+if not os.path.exists('./charts'):
+    os.mkdir('./charts')
+
+if not os.path.exists('./charts/skl_distributions'):
+    os.mkdir('./charts/skl_distributions')
+
+mod_paths = ['aaa', 'bbb', 'ccc', 'ddd', 'eee', 'fff', 'ggg']
+
+clf_paths = ['DT', 'KNN', 'NB', 'NN', 'RF', 'SVM']
+tch_paths = ['0', 'AE', 'KMEANS', 'PCA', 'SMOTE']
+
+metrics = ['accuracy', 'fscore', 'precision', 'recall']
+
+for mod in mod_paths:
+
+    mod_master = pd.DataFrame(columns=['module', 'attributes', 'classifier', 'technique', 'metric', 'score'])
+
+    for clf in clf_paths:
+        for tch in tch_paths:
+
+            clf_fname = clf.lower()
+            tch_fname = 'base' if tch == '0' else tch.lower()
+
+            df = pd.read_csv(f'./results/{clf}/{clf}-{tch}/{mod}_{clf_fname}-{tch_fname}_results.csv')
+            
+            mod_master = mod_master.append(df)
+
+    for met in metrics:
+
+        met_df = mod_master[mod_master['metric'] == met].copy(deep=True)
+
+        leq_1 = len(met_df[(met_df['score'] >= 0.0) & (met_df['score'] <= 0.1)].index)
+        leq_2 = len(met_df[(met_df['score'] > 0.1) & (met_df['score'] <= 0.2)].index)
+        leq_3 = len(met_df[(met_df['score'] > 0.2) & (met_df['score'] <= 0.3)].index)
+        leq_4 = len(met_df[(met_df['score'] > 0.3) & (met_df['score'] <= 0.4)].index)
+        leq_5 = len(met_df[(met_df['score'] > 0.4) & (met_df['score'] <= 0.5)].index)
+        leq_6 = len(met_df[(met_df['score'] > 0.5) & (met_df['score'] <= 0.6)].index)
+        leq_7 = len(met_df[(met_df['score'] > 0.6) & (met_df['score'] <= 0.7)].index)
+        leq_8 = len(met_df[(met_df['score'] > 0.7) & (met_df['score'] <= 0.8)].index)
+        leq_9 = len(met_df[(met_df['score'] > 0.8) & (met_df['score'] <= 0.9)].index)
+        leq_10 = len(met_df[(met_df['score'] > 0.9) & (met_df['score'] <= 1.0)].index)
+
+        data = [['0.0 - 0.1', leq_1],
+                ['0.1 - 0.2', leq_2],
+                ['0.2 - 0.3', leq_3],
+                ['0.3 - 0.4', leq_4],
+                ['0.4 - 0.5', leq_5],
+                ['0.5 - 0.6', leq_6],
+                ['0.6 - 0.7', leq_7],
+                ['0.7 - 0.8', leq_8],
+                ['0.8 - 0.9', leq_9],
+                ['0.9 - 1.0', leq_10]]
+
+        counts = pd.DataFrame(columns=['bucket', 'count'], data=data)
+
+        sns.set(style='darkgrid')
+
+        if met == 'accuracy':
+            color = '#4682b4'
+        elif met == 'fscore':
+            color = '#ff8c00'
+        elif met == 'precision':
+            color = '#90ee90'
+        elif met == 'recall':
+            color = '#b22222'
+
+        mod_plot = sns.barplot(x='bucket', y='count', color=color, data=counts)
+        mod_plot.set_title(f"{mod.upper()} : {met.capitalize()}", fontsize=50)
+        mod_plot.tick_params(labelsize=25)
+        mod_plot.set(ylim=(0,350))
+
+        plt.ylabel('')
+        plt.xlabel('')
+
+        plt.xticks(rotation=90)
+
+        mod_plot.figure.savefig(f'./charts/skl_distributions/{mod}-{met}_skl_distributions.png')
+
+        plt.clf()
+        plt.close('all')
+
+
---
+++ skl_summary.py
@@ -0,0 +1,66 @@
+import os
+
+import pandas as pd
+
+if not os.path.exists('./charts'):
+    os.mkdir('./charts')
+
+if not os.path.exists('./charts/skl_heatmaps'):
+    os.mkdir('./charts/skl_heatmaps')
+
+mod_paths = ['aaa', 'bbb', 'ccc', 'ddd', 'eee', 'fff', 'ggg']
+
+clf_paths = ['DT', 'KNN', 'NB', 'NN', 'RF', 'SVM']
+tch_paths = ['0', 'AE', 'KMEANS', 'PCA', 'SMOTE']
+
+metrics = ['accuracy', 'fscore', 'precision', 'recall']
+attributes = ['asmt', 'stdnt', 'abd', 'abi', 'asmt_stdnt', 'asmt_abd', 'asmt_abi', 'stdnt_abd', 'stdnt_abi', 'asmt_stdnt_abd', 'asmt_stdnt_abi']
+
+for mod in mod_paths:
+
+    mod_master = pd.DataFrame(columns=['module', 'attributes', 'classifier', 'technique', 'metric', 'score'])
+
+    for clf in clf_paths:
+        for tch in tch_paths:
+
+            clf_fname = clf.lower()
+            tch_fname = 'base' if tch == '0' else tch.lower()
+
+            df = pd.read_csv(f'./results/{clf}/{clf}-{tch}/{mod}_{clf_fname}-{tch_fname}_results.csv')
+            
+            mod_master = mod_master.append(df)
+
+    for met in metrics:
+
+
+        met_df = mod_master[mod_master['metric'] == met].copy(deep=True)
+
+        max_score = met_df['score'].max()
+        print('\n=========> {0} : {1} : {2:.4f}\n'.format(mod.upper(), met.upper(), max_score))
+
+        win_df = met_df[met_df['score'] == max_score].copy(deep=True)
+
+        num_wins = len(win_df.index)
+
+        atbt_labels = list(win_df['attributes'].value_counts().index)
+        atbt_counts = list(win_df['attributes'].value_counts())
+
+        clf_labels = list(win_df['classifier'].value_counts().index)
+        clf_counts = list(win_df['classifier'].value_counts())
+
+        tch_labels = list(win_df['technique'].value_counts().index)
+        tch_counts = list(win_df['technique'].value_counts())
+
+        print('\t+------------+\n\t| ATTRIBUTES |\n\t+------------+')
+        for i, v in enumerate(atbt_counts):
+            print('\t{0}\t{1:.2f}\t{2}'.format(v, v / num_wins * 100, atbt_labels[i]))
+
+        print('\n\t+-------------+\n\t| CLASSIFIERS |\n\t+-------------+')
+        for i, v in enumerate(clf_counts):
+            print('\t{0}\t{1:.2f}\t{2}'.format(v, v / num_wins * 100, clf_labels[i]))
+        
+        print('\n\t+------------+\n\t| TECHNIQUES |\n\t+------------+')
+        for i, v in enumerate(tch_counts):
+            print('\t{0}\t{1:.2f}\t{2}'.format(v, v / num_wins * 100, tch_labels[i]))
+
+print()
